08d021a5ef337b0c43d14369b8778c45671c960b
Andrew Ealovega <Andrew@Ealovega.dev>
2022-04-17 02:25:53 -0400

    Fix Traits.

--- src/lib/ld06_driver.rs
+++ src/lib/ld06_driver.rs
@@ -5,28 +5,39 @@ use anyhow::Result;
 use byteorder::ByteOrder;
 use cancellation::CancellationTokenSource;
 use parking_lot::Mutex;
-use ringbuffer::{ConstGenericRingBuffer, RingBufferWrite};
+use ringbuffer::{ConstGenericRingBuffer, RingBuffer, RingBufferExt, RingBufferRead, RingBufferWrite};
 use serialport::SerialPortType::UsbPort;
 use serialport::{DataBits, FlowControl, Parity, SerialPort, StopBits};
 
+/// A range reading from the sensor.
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Default)]
 pub struct Range {
+    /// The distance from the unit, in mm.
     pub dist: u16,
+    /// The intensity of the scan. 200 is 'average'.
     pub confidence: u8,
 }
 
+/// A single scan packet from the Lidar.
+///
+/// All angles are with clockwise respect to the arrow on the top of the unit.
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Default)]
 pub struct Scan {
+    /// The rotational speed of the unit, in degrees per second.
     pub radar_speed: u16,
+    /// The starting angle of this scan.
     pub start_angle: f32,
     /// The measured ranges.
     ///
     /// The first range angle is at [start_angle].
     pub data: [Range; 12],
+    /// The ending angle of this scan.
     pub end_angle: f32,
+    /// The timestamp of this scan. This will roll over at 30000.
     pub stamp: u16,
+    /// The CRC check from the lidar. This is currently not implimented.
     pub crc: u8,
 }
 
@@ -43,7 +54,8 @@ impl Scan {
     }
 }
 
-pub struct LD06<R: Read> {
+/// Struct providing access to the output data of an LD06 LiDAR.
+pub struct LD06<R: Read + Send> {
     port: Arc<Mutex<BufReader<R>>>,
     buff: Arc<Mutex<ConstGenericRingBuffer<Scan, 100>>>,
     cts: CancellationTokenSource,
@@ -100,7 +112,7 @@ impl LD06<Box<dyn SerialPort>> {
     }
 }
 
-impl<R: Read + Send + Sync + 'static> LD06<R> {
+impl<R: Read + Send + 'static> LD06<R> {
     pub fn from_reader(data: R) -> Self {
         LD06 {
             port: Arc::from(Mutex::new(BufReader::new(data))),
@@ -109,11 +121,21 @@ impl<R: Read + Send + Sync + 'static> LD06<R> {
         }
     }
 
+    /// Creates a new background thread which begins to buffer data from the LiDAR.
+    ///
+    /// Buffered data is flushed before reads begin.
+    ///
+    /// If there was already a thread spawned by another call to listen, it is cancelled.
     pub fn listen(&mut self) {
+        self.cts.cancel(); //Cancel any previously running listeners.
+        self.cts = CancellationTokenSource::new(); //Create a new cts in case we were stopped before.
+        self.buff.lock().clear(); //Flush old buffer to avoid stale data reemerging.
+
         let ct = self.cts.token().clone();
         let reader = self.port.clone();
         let ring = self.buff.clone();
 
+        //Spawn detached thread, which is controlled by the cancellation token.
         std::thread::spawn(move || {
             let mut buf: Vec<u8> = Vec::with_capacity(47); //Size of a packet
             let mut reader = reader.lock(); //Locking forever is fine cause we never read outside of here
@@ -122,6 +144,12 @@ impl<R: Read + Send + Sync + 'static> LD06<R> {
                 let mut packet = Scan::default();
                 //Read until start of next packet. This means the header of the next packet is at the end of the buffer now.
                 reader.read_until(0x54, &mut buf).unwrap(); //Panic is fine here, as it just kills background thread
+
+                // On the first read, we may not get a full packet.
+                if buf.len() < 47 {
+                    buf.clear();
+                    continue;
+                }
 
                 //See docs/refrence.pdf for packet format
                 packet.radar_speed = byteorder::BE::read_u16(&buf[1..=2]);
@@ -142,5 +170,29 @@ impl<R: Read + Send + Sync + 'static> LD06<R> {
                 buf.clear();
             }
         });
+    }
+
+    /// Stops listening to new data. Buffered data is still readable.
+    ///
+    /// It is possible to continue listening by calling [listen] again.
+    pub fn stop(&self) {
+        self.cts.cancel();
+    }
+
+    /// Reads the next scan from the buffer, if any.
+    pub fn next_scan(&mut self) -> Option<Scan> {
+        let mut lck = self.buff.lock();
+        lck.dequeue()
+    }
+
+    pub fn has_scan(&self) -> bool {
+        let lck = self.buff.lock();
+        !lck.is_empty()
+    }
+}
+
+impl<R: Read + Send> Drop for LD06<R> {
+    fn drop(&mut self) {
+        self.cts.cancel();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-
+use LDO6::ld06_driver::LD06;
 
 fn main() {
     simple_log::quick!();
+    
+    let mut ld = LD06::new_auto_port().unwrap();
+    
+    ld.listen();
     
 }
